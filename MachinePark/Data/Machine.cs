c7b840363051e1a277a606a4123c4a2f7bc44209
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-12-30 11:41:54 +0100

    Add project files.

---
+++ MachinePark/Data/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace MachinePark.Data
+{
+    public class Machine
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Data { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
